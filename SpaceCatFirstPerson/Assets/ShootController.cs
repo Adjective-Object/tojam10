e80a3a6044f22c61806e4ef2b0350cc2233e3fdd
Max <mhuan13@gmail.com>
2015-05-02 13:27:07 -0400

    added shooting back in

--- SpaceCatFirstPerson/Assets/ShootController.cs
+++ SpaceCatFirstPerson/Assets/ShootController.cs
@@ -5,15 +5,21 @@ public class ShootController : MonoBehaviour {
 
 	public string shootButton;
 	public Animator animatedThing;
+	public Gun gun = new CatSpreadGun();
 
 	// Use this for initialization
 	void Start () {
-	
+		this.gun.Init();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		bool shooting = Input.GetButton(shootButton);
 		animatedThing.SetBool("shoot", shooting);
+	}
+	
+	public void ShootBullet () {
+		Debug.Log("Shoot!");
+		this.gun.Shoot(this);
 	}
 }
--- SpaceCatFirstPerson/Assets/lookDownRenderGun.cs
+++ SpaceCatFirstPerson/Assets/lookDownRenderGun.cs
@@ -6,14 +6,12 @@ public class lookDownRenderGun : MonoBehaviour {
 	public Transform camTrans;
 	public float lookRange = 10;
 	public float posRange = -0.1f;
-	public Gun gun = new CatSpreadGun();
-
+	
 	private Vector3 init_pos;
 
 	// Use this for initialization
 	void Start () {
 		this.init_pos = this.transform.localPosition;
-		this.gun.Init();
 	}
 	
 	// Update is called once per frame
@@ -21,10 +19,6 @@ public class lookDownRenderGun : MonoBehaviour {
 		float angle = this.camTrans.rotation.eulerAngles.x;
 		this.transform.localPosition = this.init_pos +
 			new Vector3(0, angle / lookRange * posRange, 0);
-	}
-	
-	public void ShootBullet () {
-		this.gun.Shoot(this);
 	}
 
 }
