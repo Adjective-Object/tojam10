5ce225f4ecd40a5163dd5ceba365a5240ec99ef1
Artur Kink <artur.kink@gmail.com>
2015-05-03 13:41:03 -0400

    Rework of walls. Panels have a flicker shader.

---
+++ SpaceCatFirstPerson/Assets/space walls/PanelScript.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PanelScript : MonoBehaviour
+{
+    public Texture[] FullPanelBackgrounds;
+    public Texture[] FullPanels;
+
+    public Texture[] LeftPanelBackgrounds;
+    public Texture[] LeftPanels;
+
+    public Texture[] RightPanelBackgrounds;
+    public Texture[] RightPanels;
+
+
+    System.Random rand;
+    // Use this for initialization
+    void Start()
+    {
+        rand = new System.Random();
+        Debug.Log("Gonna make some panels");
+        switch (rand.Next(0, 3))
+        {
+            case 0: //Full panel
+                GetComponent<Renderer>().material.SetTexture("_MainTex", FullPanelBackgrounds[0]);
+                GetComponent<Renderer>().material.SetTexture("_PanelTex", FullPanels[0]);
+                break;
+            case 1:
+                GetComponent<Renderer>().material.SetTexture("_MainTex", LeftPanelBackgrounds[0]);
+                GetComponent<Renderer>().material.SetTexture("_PanelTex", LeftPanels[0]);
+                break;
+            case 2:
+                GetComponent<Renderer>().material.SetTexture("_MainTex", RightPanelBackgrounds[0]);
+                GetComponent<Renderer>().material.SetTexture("_PanelTex", RightPanels[0]);
+                break;
+            default:
+                break;
+        }
+    }
+
+    private float getColorOffset()
+    {
+        return (float)((rand.NextDouble() - 0.5) * 0.15);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        GetComponent<Renderer>().material.SetColor("_PanelColor", new Color(0.9f + getColorOffset(), 0.70f + getColorOffset(), 0.2f + getColorOffset()));
+    }
+}
---
+++ SpaceCatFirstPerson/Assets/space walls/WallScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallScript : MonoBehaviour {
+
+    public Texture[] wallTextures;
+
+	// Use this for initialization
+	void Start () {
+        System.Random random = new System.Random();
+        GetComponent<Renderer>().material.SetTexture("_MainTex", wallTextures[random.Next(0, wallTextures.Length)]);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
