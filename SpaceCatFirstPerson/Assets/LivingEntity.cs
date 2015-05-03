251c992a52ec79e568eddcf1ae2be40b380fe093
Max <mhuan13@gmail.com>
2015-05-02 23:07:41 -0400

    GIBS

--- SpaceCatFirstPerson/Assets/LivingEntity.cs
+++ SpaceCatFirstPerson/Assets/LivingEntity.cs
@@ -5,17 +5,42 @@ public class LivingEntity : MonoBehaviour {
 
 	public int health;
 
+	public string[] gibSpritePaths;
+	private Sprite[] gibSprites;
+	private Object gibPrefab = null;
+	
+	public float hspread = 0.1f;
+	public float vmin = 1;
+	public float vmax = 3;
+
 	// Use this for initialization
 	void Start () {
-	
+		gibSprites = new Sprite[gibSpritePaths.Length];
+		for(int i=0; i<gibSpritePaths.Length; i++) {
+			gibSprites[i] = Resources.Load<Sprite>(gibSpritePaths[i]);
+			Debug.Log(gibSpritePaths[i] +"\t"+gibSprites[i]);
+		}
+		gibPrefab = Resources.Load("gibPrefab");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
 	}
 	
 	public void damageFor(int dmg) {
 		this.health -= dmg;
+	}
+	
+	public void SpewGibs(int gibsCount) {
+		for (int i=0; i<gibsCount; i++) {
+			GameObject instance = (GameObject) Object.Instantiate(gibPrefab);
+			instance.transform.position = this.transform.position;
+			instance.GetComponent<SpriteRenderer>().sprite = 
+				gibSprites[Random.Range(0, gibSprites.Length)] ;
+			instance.GetComponent<Rigidbody>().velocity = 
+				Vector3.forward * Random.Range(-hspread, hspread) +
+				Vector3.right * Random.Range(-hspread, hspread) +
+				Vector3.up * Random.Range(vmin,vmax);
+		}
 	}
 }
---
+++ SpaceCatFirstPerson/Assets/StopOnFloor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StopOnFloor : MonoBehaviour {
+
+	public float floor = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (this.transform.position.y < floor) {
+			this.transform.position = new Vector3(
+				this.transform.position.x,
+				floor,
+				this.transform.position.z);
+			Rigidbody body = this.GetComponent<Rigidbody>();
+			if (body != null) body.velocity = Vector3.zero;
+			Destroy(this);
+			Destroy(body);
+		}
+
+	}
+}
